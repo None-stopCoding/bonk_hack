c2d782726f9d4013769234b80b002175047467a5
OlegParfenov <55343121+OlegParfenov@users.noreply.github.com>
2021-04-10 22:21:15 +0300

    x

--- server/projects/projects.py
+++ server/projects/projects.py
@@ -1,4 +1,4 @@
-from server.projects.templates import GET_PROJECT_BY_STATUS, GET_ALL_INFO_ABOUT_PROJECT, CREATE_PROJECT, CHANGE_PROJECT_STATUS
+from server.projects.templates import *
 from server.generals import Database
 
 
@@ -12,4 +12,6 @@ def create_project(name, documents, author, mentor):
      return Database().SqlQuery(CREATE_PROJECT, name, documents, author, mentor, author)
 
 def change_project_status(status, user_id, project_id):
+    if status == 2:
+         Database().SqlQuery(ADD_MENTOR, project_id, project_id)
     return Database().SqlQuery(CHANGE_PROJECT_STATUS, status, user_id, project_id)
