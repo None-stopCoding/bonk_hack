a012009bded1ea66bc047ddb5aa82f2706c59343
magnitola <nikita.lukyanow@gmail.com>
2021-04-10 14:29:01 +0300

    fix main

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run()
