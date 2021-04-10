b1494e949e473ea3632e213ca1b6e2a841840941
svyat935 <svyat935@mail.ru>
2021-04-10 22:02:24 +0300

    Added profile

--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {AuthContext} from '../../context/AuthContext';
+import List from '../../components/List';
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import {useHttp} from "../../hooks/http.hook";
+
+const personalInformation = () => {
+    const auth = useContext(AuthContext);
+    const {request} = useHttp();
+
+    const getPersonalInfo = async (status) => {
+        const data = await request('', 'POST', {
+
+        });
+        debugger;
+
+    }
+
+    return (
+
+    )
+}
 
 const Profile = () => {
     return (
       <div>
-        Profile
+          <Container maxWidth="sm">
+              <Grid item xs={4}>
+                
+              </Grid>
+              <Grid item xs={4}>
+
+              </Grid>
+              <Grid item xs={4}>
+
+              </Grid>
+          </Container>
       </div>
     );
 }
