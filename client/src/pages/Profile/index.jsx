36483e5d409bfcdc9f78f40fdf6333d92a40aa37
svyat935 <svyat935@mail.ru>
2021-04-11 00:10:09 +0300

    client/src/pages/*

--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -14,13 +14,61 @@ const PersonalInformation = () => {
         const data = await request('/api/profile/get', 'POST', {
             user_id: auth.userId
         });
-        debugger;
+        // debugger;
         setInfo(data);
     }
+
 
     useEffect(() => {
         getPersonalInfo();
     })
+
+    const getCompetence = () => {
+        let listCompetence = [];
+        if (info.competences != undefined){
+            if (info.competences.length > 0) {
+                let counter = 1;
+                info.competences.forEach((competence) => {
+                    listCompetence.push(
+                        <tr>
+                            <td>{counter}</td>
+                            <td>{competence.name}</td>
+                            <td>{competence.comment}</td>
+                            <td>Название проекта Тест</td>
+                        </tr>
+                    )
+                    counter++;
+                })
+
+                return (
+                    <div className="container">
+                        <div className="row align-items-center flex-row justify-content-center">
+                            <div className="fresh-table full-color-orange">
+                                <table id="fresh-table" className="table">
+                                    <thead>
+                                    <th data-field="id">ID</th>
+                                    <th data-field="name">Название</th>
+                                    <th data-field="desc">Описание</th>
+                                    <th data-field="project">Проект</th>
+                                    </thead>
+                                    <tbody>
+                                    {listCompetence}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                );
+            }
+        }
+
+        return (
+            <div className="container">
+                <div className="row align-items-center flex-row justify-content-center">
+                </div>
+            </div>
+        )
+    }
 
     return (
         <section className="section about-section gray-bg" id="about">
@@ -28,47 +76,42 @@ const PersonalInformation = () => {
                 <div className="row align-items-center flex-row-reverse">
                     <div className="col-lg-6">
                         <div className="about-text go-to">
-                            <h3 className="dark-color">About Me</h3>
-                            <h6 className="theme-color lead">A Lead UX &amp; UI designer based in Canada</h6>
-                            <p>I <mark>design and develop</mark> services for customers of all sizes, specializing in
-                                creating stylish, modern websites, web services and online stores. My passion is to
-                                design digital user experiences through the bold interface and meaningful interactions.
+                            <h3 className="dark-color">Профиль:</h3>
+                            <h6 className="theme-color lead"></h6>
+                            <p>
+                                Сюда можно что-то вставить!
                             </p>
                             <div className="row about-list">
                                 <div className="col-md-6">
                                     <div className="media">
-                                        <label>Birthday</label>
-                                        <p>4th april 1998</p>
+                                        <label>Фамилия</label>
+                                        <p>{info.surname}</p>
                                     </div>
                                     <div className="media">
-                                        <label>Age</label>
-                                        <p>22 Yr</p>
+                                        <label>Имя</label>
+                                        <p>{info.name}</p>
                                     </div>
                                     <div className="media">
-                                        <label>Residence</label>
-                                        <p>Canada</p>
-                                    </div>
-                                    <div className="media">
-                                        <label>Address</label>
-                                        <p>California, USA</p>
+                                        <label>Отчество</label>
+                                        <p>{info.second_name}</p>
                                     </div>
                                 </div>
                                 <div className="col-md-6">
                                     <div className="media">
-                                        <label>E-mail</label>
-                                        <p>info@domain.com</p>
+                                        <label>Тест 1</label>
+                                        <p>Тест 1</p>
                                     </div>
                                     <div className="media">
-                                        <label>Phone</label>
-                                        <p>820-885-3321</p>
+                                        <label>Тест 1</label>
+                                        <p>Тест 1</p>
                                     </div>
                                     <div className="media">
-                                        <label>Skype</label>
-                                        <p>skype.0404</p>
+                                        <label>Тест 1</label>
+                                        <p>Тест 1</p>
                                     </div>
                                     <div className="media">
-                                        <label>Freelance</label>
-                                        <p>Available</p>
+                                        <label>Тест 1</label>
+                                        <p>Тест 1</p>
                                     </div>
                                 </div>
                             </div>
@@ -82,33 +125,28 @@ const PersonalInformation = () => {
                 </div>
                 <div className="counter">
                     <div className="row">
-                        <div className="col-6 col-lg-3">
+                        <div className="col-4 col-lg-4">
                             <div className="count-data text-center">
-                                <h6 className="count h2" data-to="500" data-speed="500">500</h6>
-                                <p className="m-0px font-w-600">Happy Clients</p>
+                                <h6 className="count h2" data-to="500" data-speed="500">2</h6>
+                                <p className="m-0px font-w-600">Проектов в работе</p>
                             </div>
                         </div>
-                        <div className="col-6 col-lg-3">
+                        <div className="col-4 col-lg-4">
                             <div className="count-data text-center">
-                                <h6 className="count h2" data-to="150" data-speed="150">150</h6>
-                                <p className="m-0px font-w-600">Project Completed</p>
+                                <h6 className="count h2" data-to="150" data-speed="150">3</h6>
+                                <p className="m-0px font-w-600">Успешно завершенных</p>
                             </div>
                         </div>
-                        <div className="col-6 col-lg-3">
+                        <div className="col-4 col-lg-4">
                             <div className="count-data text-center">
-                                <h6 className="count h2" data-to="850" data-speed="850">850</h6>
-                                <p className="m-0px font-w-600">Photo Capture</p>
-                            </div>
-                        </div>
-                        <div className="col-6 col-lg-3">
-                            <div className="count-data text-center">
-                                <h6 className="count h2" data-to="190" data-speed="190">190</h6>
-                                <p className="m-0px font-w-600">Telephonic Talk</p>
+                                <h6 className="count h2" data-to="850" data-speed="850">10</h6>
+                                <p className="m-0px font-w-600">Количество компетенций</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            {getCompetence()}
         </section>
     )
 }
@@ -116,7 +154,7 @@ const PersonalInformation = () => {
 const Profile = () => {
     const role = 2;
     const information = () => {
-        if (role === 1){
+        if (role === 1) {
             return (
                 <div>
                     <Grid item xs={6}>
