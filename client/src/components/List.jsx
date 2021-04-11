39cd78b5136e021073ab564d03845e2a6f865efb
Алексей Жуков <gkvalex7@gmail.com>
2021-04-11 09:33:11 +0300

    actions

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -5,25 +5,131 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/Inbox';
 import DraftsIcon from '@material-ui/icons/Drafts';
+import Button from '@material-ui/core/Button';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 import {useHttp} from '../hooks/http.hook';
 import {AuthContext} from '../context/AuthContext';
 
-const CustomList = ({ items, onItemClick }) => {
-    const {request} = useHttp();
+const getContentByOption = (item, option, contentOptions, reload) => {
+    switch (option) {
+        case 'project': return <ProjectItem {...{ item, ...contentOptions, reload }}/>
+    }
+};
+
+const useStyles = makeStyles((theme) => ({
+    projectItem: {
+        display: 'flex',
+        position: 'relative',
+        width: '100%'
+    },
+    projectItemAuthor: {
+        position: 'absolute',
+        left: 400,
+        width: '100%'
+    },
+    projectItemButtons: {
+        alignSelf: 'flex-end'
+    },
+    projectItemButton: {
+        marginLeft: 10
+    }
+  }));
+
+const ProjectItem = ({ item, status, reload }) => {
+    const classes = useStyles();
+    const { request } = useHttp();
     const auth = useContext(AuthContext);
 
+    const getActionsByRole = (project) => {
+        const requestDefaultBody = {
+            userId: auth.userId,
+            projecId: project.id
+        };
+
+        switch (auth.role) {
+            case 'Student':
+                switch (status) {
+                    case 'Заявки':  return [
+                        {
+                            color: 'default',
+                            name: 'Принять',
+                            action: () => request('/api/project/student/accept', 'POST', requestDefaultBody)
+                        }, {
+                            color: 'red',
+                            name: 'Отклонить',
+                            action: () => request('/api/project/student/drop', 'POST', requestDefaultBody)
+                        }
+                    ];
+                    case 'Выполнение': return [
+                        {
+                            name: 'Сдать',
+                            action: () => request('/api/project/student/send', 'POST', requestDefaultBody)
+                        }
+                    ];
+                    default: return [] 
+                }
+            case 'Project manager':
+                switch (status) {
+                    case 'Новые проекты': return [
+                        {
+                            name: 'Пригласить'
+                        }, {
+                            name: 'Передать'
+                        }, {
+                            name: "Удалить"
+                        }
+                    ]
+                }
+            case 'Mentor': return []; break;
+        }
+    };
+
+    const handleContextMenuAction = async (event, action) => {
+        event.stopPropagation();
+        const actionResult = await action();
+        reload();
+    }
+
+    return (
+        <div className={classes.projectItem}>
+            <ListItemIcon>
+                <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary={item.name} />
+            <ListItemText primary={item.author} className={classes.projectItemAuthor}/>
+            <div className={classes.projectItemButtons}>
+            {
+                getActionsByRole(item).map((button) => {
+                    return (
+                        <Button
+                            variant="contained"
+                            color={button.color}
+                            className={classes.projectItemButton}
+                            startIcon={button.startIcon}
+                            endIcon={button.endIcon}
+                            onClick={(e) => handleContextMenuAction(e, button.action)}
+                        >
+                            {button.name}
+                        </Button>
+                    )
+                })
+            }  
+            </div>
+        </div>
+    );
+};
+
+const CustomList = ({ items, onItemClick, content, contentOptions, reload }) => {
     return (
         <List component="nav" aria-label="main mailbox folders">
             {
                 items.map((item, index) => {
                     return (
                         <ListItem button key={index} onClick={() => onItemClick(item)}>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary={item.name} />
-                            <ListItemText primary={item.author} />
+                            {
+                                getContentByOption(item, content, contentOptions, reload)
+                            }
                         </ListItem>
                     )
                 })
